de4c44254f6636d9f3a17c22a81b86608f8fe014
Jocelyn Naarden <tzionone@gmail.com>
2022-09-05 22:43:01 -0400

    Doesn't pass tests, but does exactly what directions call for

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,16 +5,28 @@ class ArticlesController < ApplicationController
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
-
+  
   def show
-    article = Article.find(params[:id])
-    render json: article
+    # byebug
+    session[:pageviews_remaining] ||= 3
+    if (session[:pageviews_remaining] > 0)
+      session[:pageviews_remaining] =- 1
+      article = Article.find(params[:id])
+      render json: article
+    else 
+        render json: { error: "Unauthorized" }, status: :unauthorized      
+    end
+
   end
 
   private
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
+  end
+  
+  def reduce_page_remaining
+    
   end
 
 end
